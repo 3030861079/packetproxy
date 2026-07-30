78b5abda62aaa5eedc21c449898f549eb0b400a3
PacketProxy Builder <packetproxy@example.com>
2026-07-31 00:45:01 +0800

    Fix: two-pass build with reliable sed + thorough cache clearing

--- p4a_recipes/hostpython3/__init__.py
+++ p4a_recipes/hostpython3/__init__.py
@@ -1,21 +1,9 @@
-from pythonforandroid.recipes.hostpython3 import Hostpython3Recipe as Base
+from pythonforandroid.recipes.hostpython3 import Hostpython3Recipe as Original
 
 
-class HostPython3Recipe(Base):
-    """Override default hostpython3 to pin Python 3.10.13.
-
-    Python 3.13+ removed 'cgi' which breaks Cython 0.29.x used by Kivy <=2.2.1.
-    """
+class Hostpython3Recipe(Original):
     version = "3.10.13"
     url = "https://www.python.org/ftp/python/{version}/Python-{version}.tar.xz"
-    name = "hostpython3"
-
-    def get_dir_name(self):
-        return "hostpython3-{}".format(self.version)
-
-    @property
-    def should_build(self):
-        return True
 
 
-recipe = HostPython3Recipe()
+recipe = Hostpython3Recipe()
